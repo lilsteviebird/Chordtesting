a8aed258f77103b15430b24b465875891d020f64
lilsteviebird <stevenli880@gmail.com>
2017-09-04 21:04:09 +0100

    chord editor fix

--- src/components/ChordEditor.js
+++ src/components/ChordEditor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Breadcrumb } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
 import ChordSheetJS from 'chordsheetjs';
 
 class ChordEditor extends Component {
@@ -9,7 +9,7 @@ class ChordEditor extends Component {
   }
 
   handleChange(event) {
-  const song = {...this.props.song};
+  const song = {...this.props.item};
   song.chordpro = event.target.value;
 
   this.props.updateSong(song);
@@ -18,26 +18,28 @@ class ChordEditor extends Component {
   getChordMarkup() {
     const formatter = new ChordSheetJS.HtmlFormatter();
     const parser = new ChordSheetJS.ChordProParser();
-    const song = parser.parse(this.props.song.chordpro);
+    const song = parser.parse(this.props.item.chordpro);
 
     return { __html: formatter.format(song) };
   }
 
   render() {
+    const { item: song } = this.props
+   
     return (
       <div>
         <ul className="pt-breadcrumbs">
-          <li><Breadcrumb href="/songs" text="Songs" /></li>
-          <li><Breadcrumb href="#" text={this.props.song.title} /></li>
+          <li><Link to="/songs" className="pt-breadcrumb">Songs</Link></li>
+          <li><Link to="#" className="pt-breadcrumb">{song.title}</Link></li>
         </ul>
-        <h2 style={{margin: "0.5em 0"}}>{this.props.song.title}</h2>
+        <h2 style={{margin: "0.5em 0"}}>{song.title}</h2>
         <div className="chord-editor">
           <div className="panel">
             <h3>Input</h3>
             <textarea
               style={{width: "100%", height: "100%"}}
               onChange={this.handleChange}
-              value={this.props.song.chordpro}/>
+              value={song.chordpro}/>
           </div>
           <div className="panel">
             <h3>Output</h3>
@@ -53,7 +55,6 @@ class ChordEditor extends Component {
 }
 
 export default ChordEditor;
-
 
 
 
